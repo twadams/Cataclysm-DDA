d80ddce6bcd18670e36626262766c5d7722033f5
Sergey Alirzaev <zl29ah@gmail.com>
2020-03-20 08:51:11 +0300

    Account for the rapid strike technique in DPS calc (#38880)
    # Conflicts:
    #	src/item.cpp

--- src/item.cpp
+++ src/item.cpp
@@ -1362,6 +1362,14 @@ void item::basic_info( std::vector<iteminfo> &info, const iteminfo_query *parts,
         if( parts->test( iteminfo_parts::BASE_MOVES ) ) {
             info.push_back( iteminfo( "BASE", _( "Moves per attack: " ), "",
                                       iteminfo::lower_is_better, attack_time() ) );
+            double dps = ( dmg_bash + dmg_cut + dmg_stab ) * to_moves<int>( 1_seconds ) /
+                         static_cast<double>( attack_time() );
+            static const matec_id rapid_strike( "RAPID" );
+            if( has_technique( rapid_strike ) ) {
+                dps *= 100.0 / 66;
+            }
+            info.push_back( iteminfo( "BASE", _( "Damage per second: " ), "",
+                                      iteminfo::is_decimal, dps ) );
         }
     }
 
